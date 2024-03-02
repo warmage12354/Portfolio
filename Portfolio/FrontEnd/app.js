ab5d841489af3008cb8ef1bd528f35d296009220
Brian Neisen <81530333+warmage12354@users.noreply.github.com>
2024-03-02 03:29:59 -0600

    Add files via upload

---
+++ Portfolio/FrontEnd/app.js
@@ -0,0 +1,32 @@
+
+
+
+// declaring variables
+
+
+
+// simple methods that use that function "scrollIntoView" to easily access parts of the page
+function scrollAboutMe() { 
+    let aboutMe = document.getElementById("aboutMe");
+    aboutMe.scrollIntoView();
+
+} 
+
+function scrollToDoList() { 
+    let toDo = document.getElementById("toDO")
+    toDo.scrollIntoView();
+
+} 
+
+function scrollWorkoutTracker() { 
+    let Workout = document.getElementById("Workout") 
+    Workout.scrollIntoView();
+
+} 
+
+function scrollCurConverter() { 
+    let Currency = document.getElementById("Currency")
+    Currency.scrollIntoView();
+}
+
+
